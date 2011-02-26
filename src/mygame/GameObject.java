2e567d69ea94baa94238eb81ab4ae0f69f6a9472
seth1010 <ty@pre-alpha.com>
2011-02-26 15:21:09 -0800

    commiting from my desktop

---
+++ src/mygame/GameObject.java
@@ -0,0 +1,13 @@
+package mygame;
+
+import org.newdawn.slick.Image;
+import org.newdawn.slick.SlickException;
+
+public class GameObject extends Image
+{	
+	public GameObject(String filePath) throws SlickException
+	{
+		super(filePath);
+		
+	}
+}
