c120983691830bd32d24d0fb0ce1f3dd7ee71ba6
seth1010 <ty@pre-alpha.com>
2011-02-26 16:17:40 -0800

    Update GameObject.java to extend Image instead of having its own weird implementation of the Image class.

--- src/mygame/GameObject.java
+++ src/mygame/GameObject.java
@@ -5,9 +5,12 @@ import org.newdawn.slick.SlickException;
 
 public class GameObject extends Image
 {	
+	public float scale = 1.0f;
+	public double x = 50;
+	public double y = 60;
+	
 	public GameObject(String filePath) throws SlickException
 	{
 		super(filePath);
-		
 	}
 }
